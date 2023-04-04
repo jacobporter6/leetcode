2ec70e67116ebceb2f2bc0f3b21eb30ff2a501b0
jp-byte-trading <jacob@byte-trading.com>
2023-04-04 15:52:48 +0100

    Added coin change problem

---
+++ algorithms/coin_change.py
@@ -0,0 +1,69 @@
+# https://leetcode.com/problems/coin-change/
+
+# You are given an integer array coins representing coins 
+# of different denominations and an integer amount 
+# representing a total amount of money.
+
+# Return the fewest number of coins that you need to make up that amount. 
+# If that amount of money cannot be made up by any 
+# combination of the coins, return -1.
+
+# You may assume that you have an infinite number of each kind of coin.
+from collections import deque
+from typing import Deque
+
+
+def get_coin_number(coins: list[int], amount: int) -> int:
+    """
+    Get number of coins required from array to make up amount
+    """
+    # I know that in terms of box stacking algorithms you should start biggest
+    # and work to smallest, so as a first try I would reverse sort my list 
+    # and attemt to fill using largest coins to smallest
+    coins = deque(sorted(coins, reverse=True))
+
+    def _get_coin_number(coins: Deque[int], amount: int) -> int:
+        count = 0
+        sum_ = 0
+
+        for coin in coins:
+            while 1:
+                if sum_ == amount:
+                    return count
+
+                if (new_sum := sum_ + coin) <= amount:
+                    sum_ = new_sum
+                    count += 1
+
+                else:
+                    break
+
+        return -1
+
+    while len(coins) > 0:
+        if (coin_number := _get_coin_number(coins, amount)) == -1:
+            coins.popleft()
+
+        else:
+            return coin_number
+
+    return -1
+
+
+import pytest
+
+@pytest.mark.parametrize(
+    "test_input,amount,expected",
+    [
+        ([1,2,5], 11, 3),
+        ([2], 3, -1),
+        ([1], 0, 0),
+        ([3, 5], 6, 2),
+    ]
+)
+def test_get_coin_number(
+    test_input: list[int],
+    amount: int,
+    expected: int,
+):
+    assert get_coin_number(test_input, amount) == expected
