5145f64c6698a20b07c2740108f7965506cc20a0
jp-byte-trading <jacob@byte-trading.com>
2023-04-19 21:41:10 +0100

    climbing stairs

---
+++ algorithms/climbing_stairs.py
@@ -0,0 +1,33 @@
+"""
+https://leetcode.com/problems/climbing-stairs/
+
+You are climbing a staircase. It takes n steps to reach the top.
+
+Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
+"""  # noqa: E501
+import pytest
+
+
+def climbing_stairs(n: int) -> int:
+    """
+    Climbing stairs
+    """
+    ni, nj = 0, 1
+    for _ in range(n):
+        n = ni + nj
+        ni = nj
+        nj = n
+
+    return n
+
+
+@pytest.mark.parametrize(
+    "test_input,expected",
+    [
+        (2, 2),
+        (3, 3),
+        (4, 5),
+    ],
+)
+def test_climbing_stairs(test_input, expected):
+    assert climbing_stairs(test_input) == expected
