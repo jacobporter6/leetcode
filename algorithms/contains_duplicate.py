5a86cefd8e41abe8d433730d7f53b93ced2ac0c1
jp-byte-trading <jacob@byte-trading.com>
2023-04-23 11:20:13 +0100

    Added testcase for n=1

--- algorithms/contains_duplicate.py
+++ algorithms/contains_duplicate.py
@@ -25,6 +25,7 @@ def contains_duplicate(nums: list[int]) -> bool:
         ([1, 2, 3, 1], True),
         ([1, 2, 3, 4], False),
         ([1, 1, 1, 3, 3, 4, 3, 2, 4, 2], True),
+        ([1], False),
     ],
 )
 def test_contains_duplicate(nums: list[int], expected: bool) -> None:
